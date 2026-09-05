106dd47de4dde27b77957436e0997899cd2cd501
Elias Jääsaari <elias.jaasaari@gmail.com>
2026-09-05 15:56:06 +0000

    Isolate PLSGeomOnly implementation

--- cpp/bindings.cpp
+++ cpp/bindings.cpp
@@ -11,6 +11,7 @@
 #include "Python.h"
 #include "numpy/arrayobject.h"
 #include "rf-class-depth.h"
+#include "pls-geom.h"
 #include "rf-pca.h"
 #include "rf-rp.h"
 
@@ -55,6 +56,8 @@ static int MLANN_init(mlannIndex *self, PyObject *args) {
     self->index = new RFRP(data, n, dim);
   else if (strcmp(index_type, "PCA") == 0)
     self->index = new RFPCA(data, n, dim);
+  else if (strcmp(index_type, "PLSGeomOnly") == 0)
+    self->index = new PLSGeomOnly(data, n, dim);
   else
     self->index = new RFClass(data, n, dim);
 
